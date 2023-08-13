f80a877ee24ce4d9889ff7da6ce731fa2c8f00ed
Paul-Christian Volkmer <volkmer_p@ukw.de>
2023-08-13 12:48:31 +0200

    Update gradle, kotlin and spring boot dependency management

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     id("org.springframework.boot") version "3.1.2"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.spring") version "1.8.21"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
 }
 
 group = "de.ukw.ccc"
